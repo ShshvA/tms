40f9de455602f1f194e26ec67cbe30043c44027f
Artem <shishov1610@gmail.com>
2025-11-10 19:30:14 +0300

    Improved code readability. Added bitwise operations. Added validation.

--- hw1/Program.cs
+++ hw1/Program.cs
@@ -1,47 +1,71 @@
-﻿Console.Write("Введите целочисленное число a: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите целочисленное число b: ");
-int b = Convert.ToInt32(Console.ReadLine());
+﻿int a = GetInputValue("Введите целочисленное число a: ");
+int b = GetInputValue("Введите целочисленное число b: ");
 
-Console.WriteLine();
+Console.WriteLine($"\nВведенные числа: a = {a}, b = {b}");
 
-Console.WriteLine($"Введенные числа: a = {a}, b = {b}");
+PrintBasicOperations(a, b);
+PrintIncrementOperations(a, b);
+PrintMathOperation(a, b);
+PrintBitwiseOperations(a, b);
+int GetInputValue(string message)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int result))
+            return result;
+        else
+            Console.WriteLine("Ошибка ввода. Введите целочисленное число.");
 
-Console.WriteLine();
+    }
+}
+void PrintBasicOperations(int a, int b)
+{
+    Console.WriteLine("\nАрифметические операции:");
+    Console.WriteLine($"\tСумма a + b = {a + b}");
+    Console.WriteLine($"\tРазность a - b = {a - b}");
+    Console.WriteLine($"\tПроизведение a * b = {a * b}");
+    Console.WriteLine($"\tДеление a / b, без приведения типов = {a / b}");
+    Console.WriteLine($"\tДеление a / b, с приведением типов = {(float)a / b:F2}");
+    Console.WriteLine($"\tОстаток от деление a % b = {a % b}");
+}
+void PrintIncrementOperations(int a, int b)
+{
+    Console.WriteLine("\nОперации инкремента и декремента:");
+    int c = a, d = b;
+    Console.WriteLine($"\tПостфиксный инкремент a++ : a = {c++}");
+    Console.WriteLine($"\tНовое значение после инкремента: a  = {c}\n");
+    c = a;
+    Console.WriteLine($"\tПрефиксный инкремент ++a : a = {++c}");
+    Console.WriteLine($"\tНовое значение после инкремента: a  = {c}\n");
+    c = a;
+    Console.WriteLine($"\tПостфиксный декремент a-- : a = {c--}");
+    Console.WriteLine($"\tНовое значение после декремента: a  = {c}\n");
+    c = a;
+    Console.WriteLine($"\tПрефиксный декремент --a : a = {--c}");
+    Console.WriteLine($"\tНовое значение после декремента: a  = {c}\n");
 
-Console.WriteLine($"Сумма a + b = {a + b}");
-Console.WriteLine($"Разность a - b = {a - b}");
-Console.WriteLine($"Произведение a * b = {a * b}");
-Console.WriteLine($"Деление a / b, без приведения типов = {a / b}");
-Console.WriteLine($"Деление a / b, с приведением типов = {((float)a / b):f2}");
-Console.WriteLine($"Остаток от деление a % b = {a % b}");
-
-Console.WriteLine();
-
-int c = a;
-int d = b;
-Console.WriteLine($"Постфиксный инкремент a++ : a = {c++}");
-Console.WriteLine($"Новое значение после инкремента: a  = {c}");
-Console.WriteLine();
-c = a;
-Console.WriteLine($"Префиксный инкремент ++a : a = {++c}");
-Console.WriteLine($"Новое значение после инкремента: a  = {c}");
-Console.WriteLine();
-c = a;
-Console.WriteLine($"Постфиксный декремент a-- : a = {c--}");
-Console.WriteLine($"Новое значение после декремента: a  = {c}");
-Console.WriteLine();
-c = a;
-Console.WriteLine($"Префиксный декремент --a : a = {--c}");
-Console.WriteLine($"Новое значение после декремента: a  = {c}");
-Console.WriteLine();
-c = a;
-Console.WriteLine($"Значение выражения 2 * a++ + ( b-- - 1 ) = {2 * c++ + (d-- - 1)}");
-c = a;
-d = b;
-Console.WriteLine($"Значение выражения 2 * ++a + ( --b - 1 ) = {2 * ++c + (--d - 1)}");
-Console.WriteLine();
-
-Console.WriteLine($"Возведение в степень: {a}^{b} = {Math.Pow(a, b):f2}");
-Console.WriteLine($"Квадратный корень из числа а: {Math.Sqrt(a):f2}");
-Console.WriteLine($"Модуль разности: |{a} - {b}| = {Math.Abs(a - b)}");
+    c = a; d = b;
+    Console.WriteLine($"\tЗначение выражения 2 * a++ + ( b-- - 1 ) = {2 * c++ + (d-- - 1)}");
+    c = a; d = b;
+    Console.WriteLine($"\tЗначение выражения 2 * ++a + ( --b - 1 ) = {2 * ++c + (--d - 1)}");
+}
+void PrintMathOperation(int a, int b)
+{
+    Console.WriteLine("\nМатематические операции:");
+    Console.WriteLine($"\tВозведение в степень: {a}^{b} = {Math.Pow(a, b):F2}");
+    if (a >= 0)
+        Console.WriteLine($"\tКвадратный корень из числа a: {Math.Sqrt(a):F2}");
+    else
+        Console.WriteLine($"\tКвадратный корень из отрицательного числа {a} не вычисляется");
+    Console.WriteLine($"\tМодуль разности: |{a} - {b}| = {Math.Abs(a - b)}");
+}
+void PrintBitwiseOperations(int a, int b)
+{
+    Console.WriteLine("\nПобитовые операции:");
+    Console.WriteLine($"\tБитовый сдвиг a << 1 = {a << 1}");
+    Console.WriteLine($"\tБитовый сдвиг a >> 1 = {a >> 1}");
+    Console.WriteLine($"\tПобитовое И a & b = {a & b}");
+    Console.WriteLine($"\tПобитовое ИЛИ a | b = {a | b}");
+    Console.WriteLine($"\tПобитовое исключающее ИЛИ a ^ b = {a ^ b}");
+}
