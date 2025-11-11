8c3e8c43113017437445ed09185c0f928cc73f76
Artem <shishov1610@gmail.com>
2025-11-11 20:06:13 +0300

    Fix all.

--- hw1/Program.cs
+++ hw1/Program.cs
@@ -3,7 +3,19 @@ int b = GetInputValue("Введите целочисленное число b: "
 
 Console.WriteLine($"\nВведенные числа: a = {a}, b = {b}");
 
-Console.WriteLine($"Введенные числа: a = {a}, b = {b}");
+PrintBasicOperations(a, b);
+PrintIncrementOperations(a, b);
+PrintMathOperation(a, b);
+PrintBitwiseOperations(a, b);
+int GetInputValue(string message)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int result))
+            return result;
+        else
+            Console.WriteLine("Ошибка ввода. Введите целочисленное число.");
 
     }
 }
